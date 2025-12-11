298ff5a76984704699a4af11a69e0c595226a87c
Julian Rosse <julian@helixbass.net>
2025-12-11 17:56:58 -0500

    working on seed

--- swapi-sauvignon/tests/request.rs
+++ swapi-sauvignon/tests/request.rs
@@ -1,40 +1,36 @@
 use sauvignon::json_from_response;
+use serde_json_path::{JsonPath, NodeList};
 use swapi_sauvignon::get_schema;
 
 use shared::get_db_pool;
 
-async fn request_test(request: &str, expected: &str) {
+async fn request_test(request: &str, expected: impl FnOnce(&serde_json::Value)) {
     let db_pool = get_db_pool().await.unwrap();
     let schema = get_schema();
     let response = schema.request(request, &db_pool).await;
     let json = json_from_response(&response);
-    assert_eq!(pretty_print_json(&json), pretty_print_json(expected));
-}
-
-fn pretty_print_json(json: &str) -> String {
-    let parsed: serde_json::Value = serde_json::from_str(json).unwrap();
-    serde_json::to_string_pretty(&parsed).unwrap()
+    let json: serde_json::Value = serde_json::from_str(&json).unwrap();
+    expected(&json);
 }
 
 #[tokio::test]
 async fn test_object_field() {
     request_test(
         r#"
-            query {
-              actorKatie {
+            {
+              allPlanets {
                 name
               }
             }
         "#,
-        r#"
-            {
-              "data": {
-                "actorKatie": {
-                  "name": "Katie Cassidy"
-                }
-              }
-            }
-        "#,
+        |response| {
+            assert_eq!(_q("$.data.allPlanets.*", response).len(), 60);
+        },
     )
     .await;
+}
+
+fn _q<'a>(query: &str, response: &'a serde_json::Value) -> NodeList<'a> {
+    let path = JsonPath::parse(query).unwrap();
+    path.query(response)
 }
