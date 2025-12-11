6aee4db34f208dd1d5d8f5bbc1a7e8865eb449a0
Julian Rosse <julian@helixbass.net>
2025-12-11 10:27:38 -0500

    working on seed

--- xtask/src/seed/mod.rs
+++ xtask/src/seed/mod.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 use jiff::Timestamp;
 use serde::{de::DeserializeOwned, Deserialize};
-use serde_with::{formats::CommaSeparator, serde_as, StringWithSeparator};
+use serde_with::{formats::Separator, serde_as, StringWithSeparator};
 use tokio::fs::read_to_string;
 
 fn json_seed_file_path(file_name_root: &str) -> PathBuf {
@@ -20,14 +20,52 @@ async fn parse_json_file<TTarget: DeserializeOwned>(
 }
 
 pub async fn seed() -> anyhow::Result<()> {
-    let planets: Vec<Planet> = parse_json_file("planets").await?;
+    let planets: Vec<Planet> = parse_json_file::<Vec<PlanetNested>>("planets")
+        .await?
+        .into_iter()
+        .map(Into::into)
+        .collect();
+    println!("planets: {planets:#?}");
     unimplemented!()
 }
 
+#[derive(Debug, Deserialize)]
+struct PlanetNested {
+    fields: PlanetNestedFields,
+    #[serde(rename = "pk")]
+    id: u32,
+}
+
 #[serde_as]
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
+struct PlanetNestedFields {
+    edited: Timestamp,
+    #[serde(rename = "climate")]
+    #[serde_as(as = "StringWithSeparator::<CommaSpaceSeparator, String>")]
+    climates: Vec<String>,
+}
+
+#[derive(Debug)]
 struct Planet {
     edited: Timestamp,
-    #[serde_as(as = "StringWithSeparator::<CommaSeparator, String>")]
     climates: Vec<String>,
+    id: u32,
+}
+
+impl From<PlanetNested> for Planet {
+    fn from(value: PlanetNested) -> Self {
+        Self {
+            edited: value.fields.edited,
+            climates: value.fields.climates,
+            id: value.id,
+        }
+    }
+}
+
+struct CommaSpaceSeparator;
+
+impl Separator for CommaSpaceSeparator {
+    fn separator() -> &'static str {
+        ", "
+    }
 }
