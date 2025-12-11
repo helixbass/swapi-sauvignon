8f6cbf8e1b4a2c4b747e4c45791e6ef3688328e4
Julian Rosse <julian@helixbass.net>
2025-12-11 12:00:42 -0500

    working on seed

---
+++ sql/create_tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE planets (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  edited TIMESTAMPTZ NOT NULL,
+  created TIMESTAMPTZ NOT NULL,
+  name TEXT NOT NULL,
+  surface_water FLOAT,
+  diameter INTEGER,
+  rotation_period INTEGER,
+  gravity TEXT,
+  orbital_period INTEGER,
+  population FLOAT
+);
