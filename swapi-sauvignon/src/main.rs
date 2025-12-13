05a11289b67b5bcbb9d5e7a24dfb3f85fe882d81
Julian Rosse <julian@helixbass.net>
2025-12-13 11:40:00 -0500

    working on axum

--- swapi-sauvignon/src/main.rs
+++ swapi-sauvignon/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Arc;
+
+use sauvignon_axum::{
+    axum::{self, routing::post, Extension, Router},
+    graphql,
+};
+use tokio::net::TcpListener;
+
+use swapi_sauvignon::get_schema;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let schema = get_schema();
+
+    let app = Router::new()
+        .route("/graphql", post(graphql))
+        .layer(Extension(Arc::new(schema)));
+
+    let listener = TcpListener::bind("0.0.0.0:3001").await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
