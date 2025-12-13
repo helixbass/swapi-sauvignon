50e2345632709fbbba1e963f0d81911832b6bf51
Julian Rosse <julian@helixbass.net>
2025-12-13 12:04:58 -0500

    working on axum

--- swapi-sauvignon/src/main.rs
+++ swapi-sauvignon/src/main.rs
@@ -6,15 +6,18 @@ use sauvignon_axum::{
 };
 use tokio::net::TcpListener;
 
+use shared::get_db_pool;
 use swapi_sauvignon::get_schema;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let schema = get_schema();
+    let db_pool = get_db_pool().await?;
 
     let app = Router::new()
         .route("/graphql", post(graphql))
-        .layer(Extension(Arc::new(schema)));
+        .layer(Extension(Arc::new(schema)))
+        .layer(Extension(db_pool));
 
     let listener = TcpListener::bind("0.0.0.0:3001").await?;
     axum::serve(listener, app).await?;
