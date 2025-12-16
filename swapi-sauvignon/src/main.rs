2c32385b49b606e8696e1fb9e90426fcaead33bc
Julian Rosse <julian@helixbass.net>
2025-12-16 14:38:42 -0500

    working on database

--- swapi-sauvignon/src/main.rs
+++ swapi-sauvignon/src/main.rs
@@ -4,13 +4,13 @@ use sauvignon_axum::{axum, simple_app};
 use tokio::net::TcpListener;
 
 use shared::get_db_pool;
-use swapi_sauvignon::get_schema;
+use swapi_sauvignon::{get_database, get_schema};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let schema = get_schema();
     let db_pool = get_db_pool().await?;
-    let database = get_database(db_pool).await?;
+    let database = get_database(db_pool);
 
     axum::serve(
         TcpListener::bind("0.0.0.0:3001").await?,
--- swapi-sauvignon/tests/request.rs
+++ swapi-sauvignon/tests/request.rs
@@ -1,13 +1,14 @@
 use sauvignon::json_from_response;
 use serde_json_path::{JsonPath, NodeList};
-use swapi_sauvignon::get_schema;
+use swapi_sauvignon::{get_database, get_schema};
 
 use shared::get_db_pool;
 
 async fn request_test(request: &str, expected: impl FnOnce(&serde_json::Value)) {
     let db_pool = get_db_pool().await.unwrap();
+    let database = get_database(db_pool);
     let schema = get_schema();
-    let response = schema.request(request, &db_pool).await;
+    let response = schema.request(request, &database).await;
     let json = json_from_response(&response);
     let json: serde_json::Value = serde_json::from_str(&json).unwrap();
     expected(&json);
